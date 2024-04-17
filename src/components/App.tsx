5b342f8ad3ecdbd99c4692b892c7a94f2c76949b
Vladimir <shadrov_vladimir@mail.ru>
2024-04-17 17:08:19 +0300

    Добавлена верстка всего игрового поля

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,16 +3,28 @@ import '../index.css';
 export default function App() {
   return (
     <>
-      <div className="game-field">
-        <button className="cell">X</button>
-        <button className="cell">X</button>
-        <button className="cell">X</button>
-        <button className="cell">X</button>
-        <button className="cell">X</button>
-        <button className="cell">X</button>
-        <button className="cell">X</button>
-        <button className="cell">X</button>
-        <button className="cell">X</button>
+      <div className="game">
+        <div className="game__board">
+          <h3 className="game__status">Next player: X</h3>
+          <div className="game__field">
+            <button className="cell">X</button>
+            <button className="cell">X</button>
+            <button className="cell">X</button>
+            <button className="cell">X</button>
+            <button className="cell">X</button>
+            <button className="cell">X</button>
+            <button className="cell">X</button>
+            <button className="cell">X</button>
+            <button className="cell">X</button>
+          </div>
+        </div>
+        <div className="game__info">
+          <ol>
+            <li>
+              <button>Go to game start</button>
+            </li>
+          </ol>
+        </div>
       </div>
     </>
   );
