45774954c66cdf890e38386884c4729cccd90102
Vladimir <shadrov_vladimir@mail.ru>
2024-04-17 16:48:00 +0300

    Сделал верстку игрового поля

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import '../index.css';
+
+export default function App() {
+  return (
+    <>
+      <div className="game-field">
+        <button className="cell">X</button>
+        <button className="cell">X</button>
+        <button className="cell">X</button>
+        <button className="cell">X</button>
+        <button className="cell">X</button>
+        <button className="cell">X</button>
+        <button className="cell">X</button>
+        <button className="cell">X</button>
+        <button className="cell">X</button>
+      </div>
+    </>
+  );
+}
