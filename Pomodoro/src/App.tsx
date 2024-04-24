b8243c0824a7182fe8baae3be845b428ae6dc106
Vladimir <shadrov_vladimir@mail.ru>
2024-04-24 19:07:30 +0300

    Получил данные из Supabase

--- Pomodoro/src/App.tsx
+++ Pomodoro/src/App.tsx
@@ -1,3 +1,26 @@
 export default function App() {
+  // const myObj = {
+  //   description: 'Моя вторая задача',
+  //   status: 'Новая',
+  //   task_title: 'Задача 2',
+  // };
+  async function fetchData() {
+    const response = await fetch('https://aacceayuhyrqxhtdbfli.supabase.co/rest/v1/pomodoro', {
+      headers: {
+        apikey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhY2NlYXl1aHlycXhodGRiZmxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5NjcyMDksImV4cCI6MjAyOTU0MzIwOX0._xv368W0XrDkw1fmZtioW2PredTIDBcD_UC7PanJv_c',
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+    });
+    const data = await response.json();
+
+    console.log('Data: ', data);
+  }
+
+  fetchData();
+  console.log('Монтируюсь');
+
+  fetchData();
   return <h1>Привет React</h1>;
 }
