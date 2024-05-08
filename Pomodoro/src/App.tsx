474d5fbff52d7b6b6c88c2648c17c586755203eb
Vladimir <shadrov_vladimir@mail.ru>
2024-05-08 21:18:59 +0300

    Отрисовка списков

--- Pomodoro/src/App.tsx
+++ Pomodoro/src/App.tsx
@@ -1,14 +1,17 @@
-import Counter from './components/counter';
+//import Counter from './components/counter';
+import { useState } from 'react';
+import PostList from './components/postList';
 
 export default function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Что такое React?', body: 'React - это JS библиотека' },
+    { id: 2, title: 'Что такое JS?', body: 'JS - это язык программирования' },
+    { id: 3, title: 'Что такое Python?', body: 'Python - это тоже язык программирования' },
+  ]);
+
   return (
-    <div>
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
+    <div className="app">
+      <PostList postsData={{ title: 'Список постов', posts }} />
     </div>
   );
 }
