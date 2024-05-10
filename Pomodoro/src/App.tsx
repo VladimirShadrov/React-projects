bf623b3d4a6894e3dd1ce73cf18877a6fe6f0a0f
Vladimir <shadrov_vladimir@mail.ru>
2024-05-10 17:37:06 +0300

    Очистка решения для повторной тренировки

--- Pomodoro/src/App.tsx
+++ Pomodoro/src/App.tsx
@@ -1,41 +1,24 @@
-//import Counter from './components/counter';
-import { useState } from 'react';
-import PostList from './components/postList';
-import MyButton from './components/UI/button/button';
-import MyInput from './components/UI/input/input';
+// import { useState } from 'react';
+import './styles/post.css';
 
 export default function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'Что такое React?', body: 'React - это JS библиотека' },
-    { id: 2, title: 'Что такое JS?', body: 'JS - это язык программирования' },
-    { id: 3, title: 'Что такое Python?', body: 'Python - это тоже язык программирования' },
-  ]);
-  const [postTitle, setPostTitle] = useState('');
-  const [postBody, setPostBody] = useState('');
-
-  function createPost(event: React.MouseEvent) {
-    event.preventDefault();
-    const newPost = {
-      id: Date.now(),
-      title: postTitle,
-      body: postBody,
-    };
-    setPosts([...posts, newPost]);
-    setPostTitle('');
-    setPostBody('');
-  }
+  // const [posts, setPosts] = useState([
+  //   { id: 1, title: 'Что такое React?', body: 'React - это JS библиотека' },
+  //   { id: 2, title: 'Что такое JS?', body: 'JS - это язык программирования' },
+  //   { id: 3, title: 'Что такое Python?', body: 'Python - это тоже язык программирования' },
+  // ]);
 
   return (
     <div className="app">
-      <form>
-        <MyInput value={postTitle} onChange={(event) => setPostTitle(event.target.value)} placeholder={'Название поста'} />
-        <MyInput value={postBody} onChange={(event) => setPostBody(event.target.value)} placeholder={'Тело поста'} />
-        <MyButton onClick={createPost} disabled={false}>
-          Создать пост
-        </MyButton>
-      </form>
-
-      <PostList postsData={{ title: 'Список постов', posts }} />
+      <div className="post">
+        <div className="content">
+          <h2 className="title">1. Заголовок поста</h2>
+          <p className="description">Описание поста</p>
+        </div>
+        <div className="actions">
+          <button className="delete-btn">Удалить пост</button>
+        </div>
+      </div>
     </div>
   );
 }
