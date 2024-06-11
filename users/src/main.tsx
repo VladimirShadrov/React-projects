477240b22fa43031b15a172d6da1823cad988ff5
Vladimir <shadrov_vladimir@mail.ru>
2024-06-11 18:08:48 +0300

    Создан новый проект для выполения домашнего задания

---
+++ users/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Users from './components/users';
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Users />
+  </React.StrictMode>,
+);
