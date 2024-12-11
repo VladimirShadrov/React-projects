4587249479459c3b8704c9501679ee0ffa888353
Vladimir <shadrov_vladimir@mail.ru>
2024-12-11 12:35:12 +0300

    Технический

--- Tic-Tac-Toe/src/components/board.tsx
+++ Tic-Tac-Toe/src/components/board.tsx
@@ -12,6 +12,8 @@ const Board: React.FC<BoardProps> = ({ xIsNext, cells, onPlay }) => {
   const winner = checkWinner(cells);
   let status: string;
 
+  console.log(121);
+
   function handleCellClick(i: number) {
     if (winner || cells[i]) {
       return;
