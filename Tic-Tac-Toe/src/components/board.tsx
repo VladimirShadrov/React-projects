d4897202b245e6243e11e7fb33ad331f7acc7163
Vladimir <shadrov_vladimir@mail.ru>
2025-01-14 13:29:46 +0300

    Пересечение типов

--- Tic-Tac-Toe/src/components/board.tsx
+++ Tic-Tac-Toe/src/components/board.tsx
@@ -49,14 +49,57 @@ export default Board;
 interface IUser {
   age: number;
   name: string;
-
   printUserInfo: (age: number, name: string) => void;
 }
 
-const user: IUser = {
+interface IUserRole {
+  role: string;
+}
+
+/**
+ * Объединяет в единый интерфейс свойства
+ * интерфейсов IUser и IUserRole
+ */
+interface IUserWithRole extends IUser, IUserRole {
+  /**
+   * Кроме того есть возможность задавать дополнительные свойства,
+   * которых нет в родительских интерфейсах
+   */
+  gender: string;
+}
+
+const user: IUserWithRole = {
   age: 50,
   name: 'Вова',
   printUserInfo: (age, name) => console.log(`Пользователю ${name} ${age} лет`),
+  role: 'user',
+  gender: 'male',
 };
 
 user.printUserInfo(user.age, user.name);
+
+type User = {
+  name: string;
+  age: number;
+};
+
+type Gender = {
+  gender: string;
+};
+
+type Role = {
+  role: string;
+};
+
+// Объединяем в единый тип свойства типов User, Gender и Role
+type UserWithRoleAndGender = User & Gender & Role;
+
+// Объект, созданный на основе типа UserWithRoleAndGender
+const newUser: UserWithRoleAndGender = {
+  age: 24,
+  gender: 'male',
+  name: 'Sergey',
+  role: 'user',
+};
+
+console.log(newUser);
